6efa07394b93526fc76ba76765cf3b09182474af
Glendale Acosta <gglendale17@gmail.com>
2018-10-28 03:06:03 -0700

    First commit

---
+++ public/viewer.js
@@ -0,0 +1,108 @@
+var token = "";
+var tuid = "";
+var ebs = "";
+
+// because who wants to type this every time?
+var twitch = window.Twitch.ext;
+
+// create the request options for our Twitch API calls
+var requests = {
+    set: createRequest('POST', 'cycle'),
+    get: createRequest('GET', 'query'),
+    setUpload: upload('POST', 'upload')
+};
+
+function upload(type, method, file) {
+  console.log(file);
+  return {
+      type: type,
+      url: 'https://localhost:8081/color/' + method,
+      success: logSuccess,
+      error: logError,
+      data: {
+        file: file
+      },
+      contentType: false,
+      processData: false,
+  }
+}
+
+function createRequest(type, method) {
+
+    return {
+        type: type,
+        url: 'https://localhost:8081/color/' + method,
+        success: uploadImg,
+        error: logError
+    }
+}
+
+function setAuth(token) {
+    Object.keys(requests).forEach((req) => {
+        twitch.rig.log('Setting auth headers');
+        requests[req].headers = { 'Authorization': 'Bearer ' + token }
+    });
+}
+
+twitch.onContext(function(context) {
+    twitch.rig.log(context);
+});
+
+twitch.onAuthorized(function(auth) {
+    // save our credentials
+    token = auth.token;
+    tuid = auth.userId;
+
+    // enable the button
+    $('#cycle').removeAttr('disabled');
+
+    setAuth(token);
+    $.ajax(requests.get);
+});
+
+function updateBlock(hex) {
+    twitch.rig.log('Updating block color');
+    $('#color').css('background-color', hex);
+}
+
+function uploadImg() {
+
+}
+
+function logError(_, error, status) {
+  twitch.rig.log('EBS request returned '+status+' ('+error+')');
+}
+
+function logSuccess(hex, status) {
+  // we could also use the output to update the block synchronously here,
+  // but we want all views to get the same broadcast response at the same time.
+
+  twitch.rig.log('EBS request returned '+hex+' ('+status+')');
+}
+
+$(function() {
+
+    // when we click the cycle button
+    $('#cycle').click(function() {
+        if(!token) { return twitch.rig.log('Not authorized'); }
+        twitch.rig.log('Requesting a color cycle');
+        $.ajax(requests.set);
+    });
+
+    $('#upload').click(function() {
+      twitch.rig.log('Uploading bruh');
+      console.log('here');
+      var fd = new FormData();
+      var files = $('#file-submission')[0].files[0];
+      fd.append('image',files);
+      console.log(fd.get('image'));
+      $.ajax(upload('POST', 'upload', fd.get('image')));
+
+    })
+
+    // listen for incoming broadcast message from our EBS
+    twitch.listen('broadcast', function (target, contentType, color) {
+        twitch.rig.log('Received broadcast color');
+        updateBlock(color);
+    });
+});
